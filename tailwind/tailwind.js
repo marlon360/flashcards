8d65bb02ec34b894bdc0094b23109baf072f5037
Marlon Lückert <m.lueckert@me.com>
2019-08-10 19:02:37 +0200

    border-radius, gradient

--- tailwind/tailwind.js
+++ tailwind/tailwind.js
@@ -1,8 +1,14 @@
 module.exports = {
   theme: {
     extend: {},
+    borderRadius: {
+      'xl': '1em',
+      '2xl': '2em',
+      '3xl': '4em',
+    },
     gradients: theme => ({
       'blue-green': [theme('colors.blue.500'), theme('colors.green.500')],
+      'orange': ['#FFB550', '#FF895D'],
       'purple-blue': [theme('colors.purple.500'), theme('colors.blue.500')]
     })
   },
