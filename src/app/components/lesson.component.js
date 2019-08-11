6788d044be838b1a65c57a4b1d642c103d3fc983
Marlon Lückert <m.lueckert@me.com>
2019-08-11 11:56:42 +0200

    lesson card design

--- src/app/components/lesson.component.js
+++ src/app/components/lesson.component.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CirclePercentage from './circle-percentage/circle-percentage.component.js';
+import Play from './../icons/play.svg';
+import Pencil from './../icons/pencil.svg';
+
 function Lesson(props) {
+    const [open, setOpen] = useState(false);
+
     return (
-        <div className="w-full relative max-w-lg mb-5">
-            <div className="bg-gradient-120-white p-6 rounded-xl text-gray-800 shadow-lg flex justify-between">
+        <div className="bg-gradient-120-white p-6 rounded-xl shadow-md w-full max-w-lg mb-5">
+            <div onClick={() => setOpen(!open)} className="text-gray-800 flex justify-between">
                 <div>
                     <div className="-mb-1 text-sm">{props.cards} Karten</div>
-                    <h4 className="text-2xl font-bold truncate">{props.name}</h4>
+                    <h4 className="font-sans text-2xl font-bold truncate">{props.name}</h4>
                 </div>
                 <div className="w-12">
                     <CirclePercentage background="#aaaaaa" startColor="#75DEA8" endColor="#B3FF9D" className="w-full max-w-xs text-gray-600 float-right" percentage={props.percentage} strokeWidth="5" />
+                </div>
+            </div>
+            <div className={`${open ? 'h-44' : 'h-0'} transition-all transition-500 overflow-auto text-white font-bold text-xl`}>
+                <div className="bg-gradient-120-green-blue px-6 py-4 rounded-xl shadow-md w-full max-w-lg mt-5 flex">
+                    <div className="w-12">
+                        <img src={Play} alt="Start"></img>
+                    </div>
+                    Lernen
+                </div>
+                <div className="bg-gradient-120-purple-blue px-6 py-4 rounded-xl shadow-md w-full max-w-lg mt-5 flex">
+                    <div className="w-12">
+                        <img src={Pencil} alt="Bearbeiten"></img>
+                    </div>
+                    Karten bearbeiten
                 </div>
             </div>
         </div>
--- tailwind/tailwind.js
+++ tailwind/tailwind.js
@@ -1,26 +1,33 @@
 module.exports = {
   theme: {
-    extend: {},
-    borderRadius: {
-      'xl': '1em',
-      '2xl': '2em',
-      '3xl': '4em',
+    extend: {
+      height: {
+        '36': '9rem',
+        '44': '11rem',
+      },
+      borderRadius: {
+        'xl': '1em',
+        '2xl': '2em',
+        '3xl': '4em',
+      },
     },
-    height: {
-      '36': '9em'
-    },
+        fontFamily: {
+          'sans': ['SF Text'],
+        },
     gradients: theme => ({
       'blue-green': [theme('colors.blue.500'), theme('colors.green.500')],
       'orange': ['#FFB550', '#FF895D'],
       'blue': ['#50CCFF', '#5D71FF'],
       'white': ['#FCFCFC', '#FFFFFF'],
-      'purple-blue': [theme('colors.purple.500'), theme('colors.blue.500')]
+      'green-blue': ["#79EABC", "#47C4F2"],
+      'purple-blue': ["#C1A6E5", "#47A6F2"]
     })
   },
   variants: {
     gradients: ['responsive', 'hover'],
   },
   plugins: [
-      require('./plugins/gradients')
+      require('./plugins/gradients'),
+      require('tailwindcss-transitions')()
   ]
 }
