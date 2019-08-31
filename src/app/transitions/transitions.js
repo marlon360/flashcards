8d005d72fd198723cd87d72b750e4330f00502af
Marlon Lückert <m.lueckert@me.com>
2019-08-31 20:14:50 +0200

    transitino zindex start values

--- src/app/transitions/transitions.js
+++ src/app/transitions/transitions.js
@@ -1,31 +1,31 @@
 export const SlideLeft = {
     transition: {
-        from: {opacity: 0.5, transform: 'translate3d(100%, 0, 0)'},
+        from: {opacity: 0.5, transform: 'translate3d(100%, 0, 0)', zIndex: 0},
         enter: {opacity: 1, transform: 'translate3d(0%, 0, 0)', zIndex: 10},
-        leave: {opacity: 0.6, transform: 'translate3d(-50%, 0, 0)'},
+        leave: {opacity: 0.6, transform: 'translate3d(-50%, 0, 0)', zIndex: 0},
     }
 } 
 
 export const SlideRight = {
     transition: {
-        from: {opacity: 0.6, transform: 'translate3d(-50%, 0, 0)'},
-        enter: {opacity: 1, transform: 'translate3d(0%, 0, 0)'},
+        from: {opacity: 0.6, transform: 'translate3d(-50%, 0, 0)', zIndex: 0},
+        enter: {opacity: 1, transform: 'translate3d(0%, 0, 0)', zIndex: 0},
         leave: {opacity: 1, transform: 'translate3d(100%, 0, 0)', zIndex: 10},
     }
 } 
 
 export const SlideOverFromBottom = {
     transition: {
-        from: {opacity: 1, transform: 'translate3d(0, 100%, 0)'},
-        enter: {opacity: 1, transform: 'translate3d(0, 0, 0)'},
-        leave: {opacity: 0.9, transform: 'translate3d(0, 0, 0)'},
+        from: {opacity: 1, transform: 'translate3d(0, 100%, 0)', zIndex: 20},
+        enter: {opacity: 1, transform: 'translate3d(0, 0, 0)', zIndex: 20},
+        leave: {opacity: 0.9, transform: 'translate3d(0, 0, 0)', zIndex: 0},
     }
 } 
 
 export const SlideDown = {
     transition: {
-        from: {opacity: 1, transform: 'translate3d(0, 0, 0)'},
-        enter: {opacity: 1, transform: 'translate3d(0, 0, 0)'},
-        leave: {opacity: 1, transform: 'translate3d(0, 100%, 0)'},
+        from: {opacity: 1, transform: 'translate3d(0, 0, 0)', zIndex: 20},
+        enter: {opacity: 1, transform: 'translate3d(0, 0, 0)', zIndex: 0},
+        leave: {opacity: 1, transform: 'translate3d(0, 100%, 0)', zIndex: 20},
     }
 } 
